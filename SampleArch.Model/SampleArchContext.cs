e0a38828f197f081120df6946da3f46ddbac575b
nhatquang-sl <sunlight479@yahoo.com>
2015-05-07 03:36:36 +0700

    First Commit

---
+++ SampleArch.Model/SampleArchContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleArch.Model
+{
+    public class SampleArchContext : DbContext
+    {
+        public SampleArchContext()
+            : base("Name=SampleArchContext")
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        public override int SaveChanges()
+        {
+            var modifiedEntries = ChangeTracker.Entries().Where(x => x.Entity is IAuditableEntity
+                && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            foreach (var entry in modifiedEntries)
+            {
+                IAuditableEntity entity = entry.Entity as IAuditableEntity;
+                if (entity != null)
+                {
+                    string identityName = Thread.CurrentPrincipal.Identity.Name;
+                    DateTime now = DateTime.UtcNow;
+
+                    if (entry.State == EntityState.Added)
+                    {
+                        entity.CreateBy = identityName;
+                        entity.CreatedDate = now;
+                    }
+                    else
+                    {
+                        base.Entry(entity).Property(x => x.CreateBy).IsModified = false;
+                        base.Entry(entity).Property(x => x.CreatedDate).IsModified = false;
+                    }
+
+                    entity.UpdatedBy = identityName;
+                    entity.UpdatedDate = now;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
